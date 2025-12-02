2e3d7effe456983ebe001601c12d5754062b029f
Arina <a.manukyan2006@gmail.com>
2025-12-02 20:18:38 +0400

    i think map is done, Mananchik you can do your mlx part, we don't disturb each other, I have to add mmini few functions, and split functions which are in main and parce.c, you have to use data.splitted_map

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: arina <arina@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 22:01:55 by arina             #+#    #+#             */
-/*   Updated: 2025/11/30 18:00:15 by arina            ###   ########.fr       */
+/*   Updated: 2025/12/02 20:17:33 by arina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -282,7 +282,6 @@ int	main(int argc, char **argv)
 	int				return_value;
 
 	return_value = 1;
-	// res = NULL;
 	if (argc == 2)
 	{
 		check_file(argv[1]);
